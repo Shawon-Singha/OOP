b03efa143636f2edf9679361fbb9217db081e297
Your Name <shawonsingha10@gmail.com>
2023-03-05 11:40:44 +0600

    update

---
+++ UML_to_code/4Person/Person3/Person3.java
@@ -0,0 +1,15 @@
+//creat constructor
+
+package Person;
+
+public class Person3 
+{
+    String name;
+    int age;
+
+    Person3(String n,int a)
+    {
+      name = n;
+      age = a;
+    }
+}
---
+++ UML_to_code/4Person/Person3/Person3A.java
@@ -0,0 +1,14 @@
+package Person;
+
+public class Person3A 
+{
+    public static void main(String[] args) 
+    {
+        Person3 p1 = new Person3("Shawon",21);
+
+        System.out.println("Name of person:"+p1.name);
+        System.out.println("Age of person:"+p1.age);
+        System.out.println("\n");
+
+    }
+}
