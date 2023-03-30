085105f195591b6f5c14f3dabe28428d1157a90e
Your Name <shawonsingha10@gmail.com>
2023-03-30 20:07:30 +0600

    update

---
+++ 8Final_Keyword/1Variable/Student.java
@@ -0,0 +1,14 @@
+package Singha;
+
+public class Student 
+{
+    final String name = "Shawon";
+    //when we use "final" keyword, it look like constant
+    int fee = 35000;
+
+    void display()
+    {
+        System.out.println("Name : "+name);
+        System.out.println("Fee : "+fee);
+    }
+}
---
+++ 8Final_Keyword/1Variable/Test.java
@@ -0,0 +1,10 @@
+package Singha;
+
+public class Test 
+{
+    public static void main(String[] args) 
+    {
+        Student s1 = new Student();
+        s1.display();
+    }
+}
