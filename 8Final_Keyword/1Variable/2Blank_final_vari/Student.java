353b265786cb712d29860e449d962f35e35742ce
Your Name <shawonsingha10@gmail.com>
2023-03-30 20:18:15 +0600

    update

---
+++ 8Final_Keyword/1Variable/2Blank_final_vari/Student.java
@@ -0,0 +1,20 @@
+package Singha;
+
+public class Student 
+{
+    final String Name; //blank final variable
+    final int age;  //blank final variable
+
+   // finale variable must initial in constructor
+    Student()
+    {
+        Name = "Shawon";
+        age = 3500;
+    }
+
+    void display()
+    {
+        System.out.println("Name: "+Name);
+        System.out.println("Age : "+age);
+    }
+}
