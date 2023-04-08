a89ab1ff3610d102087df26b132175854a0134d9
Your Name <shawonsingha10@gmail.com>
2023-04-08 23:11:21 +0600

    update

---
+++ 11Type_Casting/1Implicit/Math.java
@@ -0,0 +1,12 @@
+package Singha;
+
+/*Implicit means "byte - short - int - long - float - double" */
+
+public class Math {
+    public static void main(String[] args) {
+        int x = 10;
+        double y = x ; //lower to upper
+
+        System.out.println(y);
+    }
+}
---
+++ 11Type_Casting/2Explicit/Math1.java
@@ -0,0 +1,13 @@
+package Singha;
+
+/*double - float - long - int - short - byte */
+
+public class Math1 {
+    public static void main(String[] args) {
+        double x = 10.5;
+        int y = (int) x; /* FORMAT OF UPPER CASE TO LOWER CASE*/
+        System.out.println(y);
+    }
+    
+    
+}
