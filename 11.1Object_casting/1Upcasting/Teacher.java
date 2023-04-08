2b0194d26074dfeecaca37ae67e8d5508d31c9ac
Your Name <shawonsingha10@gmail.com>
2023-04-08 23:18:05 +0600

    update

---
+++ 11.1Object_casting/1Upcasting/Teacher.java
@@ -0,0 +1,8 @@
+package Singha;
+
+public class Teacher extends Student {
+    void display()
+    {
+        System.out.println("This is a Teacher Class.");
+    }
+}
---
+++ 11.1Object_casting/1Upcasting/Test2.java
@@ -0,0 +1,9 @@
+package Singha;
+
+public class Test2 {
+    public static void main(String[] args) {
+        
+        Student s = new Teacher(); // store sub class object inside super class
+        s.display();
+    }
+}
