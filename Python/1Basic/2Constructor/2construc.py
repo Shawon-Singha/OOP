f702a419b48f5e501d68e6ed92e14082e616a179
Shawon-Singha <shawonsingha10@gmail.com>
2024-03-22 22:25:14 +0600

    update

---
+++ Python/1Basic/2Constructor/2construc.py
@@ -0,0 +1,10 @@
+class student:
+    def __init__(self,name,marks):
+        self.name = name
+        self.marks = marks
+
+s1 = student("Shawon",8)
+print(s1.name,s1.marks)
+
+s2 = student("Anik",9)
+print(s2.name,s2.marks)
