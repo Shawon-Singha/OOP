839076f905cb33bea6b189bd8fd57fea9f8dbc73
Shawon-Singha <shawonsingha10@gmail.com>
2024-03-24 21:29:29 +0600

    update

--- Python/1Basic/2Constructor/2construc.py
+++ Python/1Basic/2Constructor/2construc.py
@@ -1,10 +1,19 @@
 class student:
+    #default constructor
+    def __init__(self):
+        pass
+
+    #parameter constructor
     def __init__(self,name,marks):
         self.name = name
         self.marks = marks
+
+
+s = student()
 
 s1 = student("Shawon",8)
 print(s1.name,s1.marks)
 
 s2 = student("Anik",9)
 print(s2.name,s2.marks)
+
