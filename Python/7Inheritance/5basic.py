50f1e8aba9d505dee6eb5f2a7ff3e43cc9b26b69
Shawon-Singha <shawonsingha10@gmail.com>
2024-03-30 01:25:03 +0600

    update

---
+++ Python/7Inheritance/5basic.py
@@ -0,0 +1,25 @@
+class person:
+    name = "Anik"
+
+    def change(self,name):
+        self.name = name
+
+p1 = person()
+p1.change("Shawon")
+print(p1.name)
+print(person.name)
+
+#try to update name in all code
+class person:
+    name = "Anik"
+
+    def change(self,name):
+        self.name = name
+        person.name = name #first method
+        self.__class__.name = name
+
+#here change the name in all place
+p1 = person()
+p1.change("Shawon")
+print(p1.name)
+print(person.name)
